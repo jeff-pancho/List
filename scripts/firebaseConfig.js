da76c6e8e5f2216a49e71d03b22127b9b7369e49
Jeff Pancho <jepoypancho@hotmail.com>
2019-11-08 13:36:17 -0800

    app config and added user to database

---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAoQxDEtP4vNI-k_E-12emT-oXEMqRXhmw",
+    authDomain: "list-615f1.firebaseapp.com",
+    databaseURL: "https://list-615f1.firebaseio.com",
+    projectId: "list-615f1",
+    storageBucket: "list-615f1.appspot.com",
+    messagingSenderId: "68229933150",
+    appId: "1:68229933150:web:223ad34b58a72c4f6aeccf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
