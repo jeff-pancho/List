5d8532a6ecf27f3204a38c46500fba1d6c13a7e0
Jeff Pancho <jepoypancho@hotmail.com>
2019-11-08 09:51:39 -0800

    reimplemented adding lists

--- scripts/addList.js
+++ scripts/addList.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-    $("#add-list").click(addList);
+    //pointer to the list container
+    var listContainer = $("#list-container");
+    //pointer to the container that holds the create list button
+    var createList = $("#create-list-container");
+    //pointer to the create list button
+    var createListButton = $("#create-list");
+    //pointer to the container that tells you if there are no lists
+    var noLists = $("#no-lists");
+    
+    $(createListButton).click(addList);
+
+    function addList() {
+        if($(noLists).is(":visible")) {
+            $(noLists).hide();
+        }
+        
+        let listItem = $("<li class=\"list-group-item\">New List</li>");
+        $(listItem).insertBefore(createList)
+    }
 });
-
-function addList() {
-    let listGroupItem = $("<li class=\"list-group-item\">TEST</li>");
-    $(".list-group").append(listGroupItem);
-    console.log("List created.");
-
-    $(".no-lists").hide();
-}
