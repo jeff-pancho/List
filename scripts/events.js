7679345c9bccb7e7d9116d743ec58ef942399221
Vincent Tran <vintran1995@gmail.com>
2019-11-21 02:02:24 -0800

    artificial event adding

--- scripts/events.js
+++ scripts/events.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-    
-})
+    // pointer to container holding the events
+    var eventContainer = $("#event-container");
+
+    // pointer to container create new event container
+    var createEvent = $("#create-event-container");
+
+    // pointer to create event button
+    var createEventButton = $("#create-event");
+
+    // pointer to "No Events" messgae
+    var noEvents = $("#no-events");
+
+    // Placeholder event
+    var eventItem = $("<li class='event-group-item'>New Event</li>");
+
+    createEventButton.click(addEvent);
+
+    function addEvent() {
+        hideNoEventsMessage();
+        createNewEvent();
+    }
+
+    function createNewEvent() {
+        console.log("hi");
+        eventItem.clone().insertBefore(createEvent);
+    }
+
+    function hideNoEventsMessage() {
+        if(noEvents.is(":visible")) {
+            noEvents.hide();
+        }
+    }
+
+});
