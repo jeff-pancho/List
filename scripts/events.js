af28c1cf21bf34f2a3536f8d736ee4ef40f78f15
Vincent Tran <vintran1995@gmail.com>
2019-11-29 09:10:35 -0800

    minor changes to datepicker

--- scripts/events.js
+++ scripts/events.js
@@ -24,10 +24,10 @@ $(document).ready(function () {
 
     // Datepicker setup
     var date_input = $('input[name="date"]'); //our date input has the name "date"
-    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+    // var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
     var options = {
         format: 'mm/dd/yyyy',
-        container: container,
+        // container: container,
         todayHighlight: true,
         autoclose: true,
     };
