50a01ba964979139ae2eb2428f54c9e5aba8a0d4
Jeff Pancho <jepoypancho@hotmail.com>
2019-12-02 20:19:15 -0800

    added tasks to list dropdown

--- scripts/list.js
+++ scripts/list.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
     const createListButton = $("#create-list");
     // pointer to the container that tells you if there are no lists
     const noLists = $("#no-lists");
-    // pointer to "Save Changes" button in modal form
-    const saveChanges = $("#save-changes");
+    // pointer to "Save Changes" button in list modal form
+    const listSaveChanges = $("#list-save-changes");
     // pointer to the "List name" form
     const listNameForm = $("#list-name");
     // pointer to the create task button inside the modal form
@@ -17,9 +17,17 @@ $(document).ready(function(){
     const taskContainer = $("#task-container");
     // pointer to the "Task name" form
     const taskNameForm = $("#task-name-form");
+    // pointer to the "Save changes" button in task modal form
+    const taskSaveChanges = $("#task-save-changes");
     // pointer to the cancel button in the modal form for tasks
     const cancelCreateTasks = $("#cancel-create-tasks");
+    // pointer to the trash can button
+    const trashCanBtn = $("#trash-can-btn");
     
+    // number of lists currently loaded
+    let listsCount = 0;
+    // current list name after inputting it into the modal form
+    let currentListName = "";
     
     //if user is authenticated
     firebase.auth().onAuthStateChanged(function(user) {
@@ -33,24 +41,22 @@ $(document).ready(function(){
                 //execute a function for each child of the lists collectin
                 doc.forEach(function(child){
                     //append before #create-list-container
-                    createNewList();
+                    createNewList("hello", []);
                 });
             }
         });
+        console.log(listsCount);
     });
 
-    $(saveChanges).click(function() {
+    $(listSaveChanges).click(function() {
         // save the value of the input
-        let listName = $(listNameForm).val();
-        console.log(listName);
-
+        currentListName = $(listNameForm).val();
         // reset the value of input
-        $(listName).val("");
+        $(listNameForm).val("");
     });
 
     $("#create-task-modal-form").click(function(e) {
         e.preventDefault();
-    // $(createTaskButton).click(function() {
         // if the "Task name" form is filled out
         if ($(taskNameForm).val()) {
             let taskName = $(taskNameForm).val();
@@ -59,14 +65,33 @@ $(document).ready(function(){
             addTaskToForm(taskName);
         }
     });
+
+    $(taskSaveChanges).click(function() {
+        // Variable to save the names of each task.
+        let tasks = [];
+        /**
+         * Iterate through each child (task item) in the task container.
+         */
+        $(taskContainer).children().each(function(i, child) {
+            let taskName = $(child).find("span").html()
+            tasks.push(taskName);
+        });
+        console.log(currentListName);
+        addList(currentListName, tasks);
+        console.log(tasks);
+        emptyTasks();
+    });
     
 
-    $(cancelCreateTasks).click(function() {
-        $(taskContainer).empty();
-    });
+    $(cancelCreateTasks).click(emptyTasks);
 
+    /**
+     * Adds a new task to the task modal form. (Phew)
+     * @param {String} taskName name of the task
+     */
     function addTaskToForm(taskName) {
         let taskItem = $("#task-clone").clone().show();
+
         $(taskItem).find("span").html(taskName);
         // trash can button to delete itself
         $(taskItem).find("button").click(function() {
@@ -78,21 +103,66 @@ $(document).ready(function(){
         $(taskContainer).scrollTop(1000000);
     }
 
-    function addList(listName) {
+    /**
+     * Adds the list to the document and saves it to the database.
+     * @param {String} listName the name of the list
+     * @param {Array} taskArray the array that holds all the names of the tasks
+     */
+    function addList(listName, taskArray) {
         hideNoLists();
         
         //append before #create-list-container
-        createNewList();
+        createNewList(listName, taskArray);
 
         //add new list to database
-        firebase.auth().onAuthStateChanged(function(user) {
-            db.collection("users").doc(user.uid).collection("lists").add({
-                "name": "test",
-                "status": "active"
-            });
+        // firebase.auth().onAuthStateChanged(function(user) {
+        //     db.collection("users").doc(user.uid).collection("lists").add({
+        //         "name": listName,
+        //         "status": "active"
+        //     });
+        // });
+    }
+
+    /**
+     * Creates the list element along with its tasks and places it into the document.
+     * @param {String} listName the name of the list
+     * @param {Array} taskArray the array that holds all the names of the tasks
+     */
+    function createNewList(listName, taskArray) {
+        let clone = $("#list-clone").clone().show();
+        $(clone).find(".down").hide();
+        $(clone).find(".list-name").html(listName);
+
+        // set unique id for the collapsible
+        $(clone).find(".collapse").attr("id", "collapse-" + listsCount);
+        // set target of the button to the unique collapsible id
+        $(clone).find(".drop-down-btn").attr("href", "#collapse-" + listsCount);
+        // bind click event to button
+        $(clone).find(".drop-down-btn").click(toggleArrow);
+
+        generateTasks(clone, taskArray)
+
+        $(clone).insertBefore(createList);
+        listsCount++;
+    }
+
+    /**
+     * Generates all the tasks and puts it all into the clone object.
+     * @param {*} clone the jQuery object
+     * @param {Array} taskArray the array that holds all the names of the tasks
+     */
+    function generateTasks(clone, taskArray) {
+        let taskListContainer = $(clone).find(".task-list-container");
+        taskArray.forEach(function(task) {
+            let taskClone = $("#task-clone").clone().show();
+            $(taskClone).find("span").html(task);
+            $(taskListContainer).append(taskClone);
         });
     }
 
+    /**
+     * Hide the notifier that says you have no lists.
+     */
     function hideNoLists() {
         //hide #no-lists if it's still visible
         if($(noLists).is(":visible")) {
@@ -100,11 +170,25 @@ $(document).ready(function(){
         }
     }
 
-    //create the list element
-    function createNewList() {
-        let listItem = $("#list-clone").clone().show();
-        $(listItem).find(".down").hide();
+    /**
+     * Empty the task items in the task container in the task modal form. (Phew)
+     */
+    function emptyTasks() {
+        $(taskContainer).empty();
+    }
 
-        $(listItem).insertBefore(createList);
+    /**
+     * Toggles the dropdown arrow's orientation.
+     */
+    function toggleArrow() {
+        // toggle between the up and down image of the dropdown button
+        console.log("test")
+        if ($(this).find(".up").is(":visible")) {
+            $(this).find(".up").hide();
+            $(this).find(".down").show();
+        } else {
+            $(this).find(".up").show();
+            $(this).find(".down").hide();
+        }
     }
 });
